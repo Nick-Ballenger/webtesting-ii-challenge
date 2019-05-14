31324f6fd5c4b74f392da0b3f8b46cee85454872
Lambda_School_Loaner_85 <lambda_school_loaner_85@Lambda-School-Loaner-85s-MacBook-Pro.local>
2019-05-14 11:06:39 -0500

    set up dashboard and display, working on logic in app

--- baseball/src/App.js
+++ baseball/src/App.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+import Dashboard from'./dashboard/dashboard'
+import Display from './display/display'
+
+class App extends Component() {
+  state = {
+    player:'JimBob',
+    balls:0,
+    strikes:0
+  }
+ 
+  resetHandler = e => {
+    this.setState({
+      balls: 0,
+      strikes: 0
+    });
+  };
+
+  hitHandler = e =>{
+    this.resetHandler(e)
+  }
+
 }
 
 export default App;
--- baseball/src/display/display.js
+++ baseball/src/display/display.js
@@ -1,8 +1,19 @@
 import React from 'react'
 
 
-const Display = props =>{
 
-}
+    const Display = props => {
+        const { player, balls, strikes, fouls } = props.atBat;
+        return (
+          <div className="Display stuffs">
+            <li data-testid="player">At-bat: {player}</li>
+            <li data-testid="balls">Balls: {balls}</li>
+            <li data-testid="strikes">Strikes: {strikes}</li>
+            <li data-testid="fouls">Fouls: {fouls}</li>
+          </div>
+        );
+      };
+      
+
 
 export default Display
