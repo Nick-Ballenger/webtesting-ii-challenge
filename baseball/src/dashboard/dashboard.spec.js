1ef38ce4460a894356c3b5935af8aa129c2a6379
Lambda_School_Loaner_85 <lambda_school_loaner_85@Lambda-School-Loaner-85s-MacBook-Pro.local>
2019-05-14 17:14:08 -0500

    left one after each in

--- baseball/src/dashboard/dashboard.spec.js
+++ baseball/src/dashboard/dashboard.spec.js
@@ -2,7 +2,6 @@ import React from "react";
 import { render, fireEvent } from "react-testing-library";
 import "jest-dom/extend-expect";
 import Dashboard from "./dashboard";
-import 'react-testing-library/cleanup-after-each';
 
 
 describe("<Dashboard/>", () => {
--- baseball/src/display/display.js
+++ baseball/src/display/display.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import 'react-testing-library/cleanup-after-each';
 
 
     const Display = props => {
