9af122b7c7730833be664264ab4db4a112fa474e
Lambda_School_Loaner_85 <lambda_school_loaner_85@Lambda-School-Loaner-85s-MacBook-Pro.local>
2019-05-14 11:18:22 -0500

    strike and hit logic implemented

--- baseball/src/App.js
+++ baseball/src/App.js
@@ -9,18 +9,34 @@ class App extends Component() {
     balls:0,
     strikes:0
   }
- 
+ //reset logic
   resetHandler = e => {
     this.setState({
       balls: 0,
       strikes: 0
     });
   };
-
+//hit logic
   hitHandler = e =>{
     this.resetHandler(e)
   }
 
+  //strike logic
+  strikeHandler = e =>{
+    if(this.state.strikes < 2){
+this.setState(previousState =>{
+  return {strikes:previousState.strikes + 1}
+})
+    }
+else{
+  this.resetHandler(e)
 }
+  }
+};
+
+//foul logic
+
+
+
 
 export default App;
